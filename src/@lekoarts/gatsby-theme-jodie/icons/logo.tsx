63385300340a976d904e931e6fa94c6150fd9c78
Landododo <1108landon@gmail.com>
2025-07-29 15:35:02 +0200

    new resume and fixed logo

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const Logo = () => (
   <img
-    src="/thinking.JPG"
+    src="/thinking.jpg"
     alt="My Logo"
     style={{ height: "100px", width: "auto" }} // customize size as needed
   />

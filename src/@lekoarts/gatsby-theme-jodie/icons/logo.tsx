e7f23e9a97a7bdacb234c8d5e1cac0a206de4085
lando <lando@mit.edu>
2026-05-31 20:14:14 -0500

    added vlog for research project and updated resume

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 
 const Logo = () => (
-  <img
+    <img
     src="/thinking.jpg"
     alt="My Logo"
     style={{ height: "100px", width: "auto" }} // customize size as needed
   />
+
 );
 
 export default Logo;
